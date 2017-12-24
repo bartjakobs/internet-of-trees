3066e118a7584236787ce5662210a50fe74f89f8
Bart Jakobs <bartjakobs@gmail.com>
2017-12-24 11:21:21 +0100

    Statistics

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -3,6 +3,41 @@
         <title>Internet of Trees</title>
 <link href="https://fonts.googleapis.com/css?family=Indie+Flower|Oswald" rel="stylesheet">
 
+        <script>
+            function request (method, url, data, callback) {
+                var params = typeof data == 'string' ? data : Object.keys(data).map(
+                    function (k) { return encodeURIComponent(k) + '=' + encodeURIComponent(data[k]) }
+                ).join('&');
+
+                var xhr = new XMLHttpRequest();
+
+                xhr.open(method, url);
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState > 3) {
+                        callback({ status: xhr.status, response: xhr.responseText })
+
+                    }
+                };
+                xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+                xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+                if (method != "GET") xhr.send(params);
+                else xhr.send(null);
+                return xhr;
+            }
+
+            window.addEventListener('load', function(){
+                request('GET', '/api/public/statistics',{}, function(result){
+                    
+                    var results = JSON.parse(result.response);
+                    document.getElementById('totalTrees').innerHTML = results.totalTrees;
+                    document.getElementById('totalOn').innerHTML = results.totalOn;
+                    document.getElementById('totalDecorations').innerHTML = results.totalDecorations;
+
+                })
+            })
+
+
+        </script>
         <link rel="stylesheet" type="text/css" href="/css/home.css">
     </head>
     <body>
@@ -17,7 +52,8 @@
             </div>
 
             <div class="row stats">
-                Currently 5 christmas trees are registered, 2 of which with burning candles, with a total of 400 decorations in those trees.
+                Currently <span id="totalTrees">5</span> christmas trees are registered, <span id="totalOn">2</span> of which with burning candles, with a total of <span id="totalDecorations">2</span> decorations in those trees.
+
         </div>
     </body>
 </html>
